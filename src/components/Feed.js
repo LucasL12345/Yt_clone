ddc61542a059ac77e4ce370ccd52042ea7f3bdf1
LucasL12345 <lucaslemoine49@gmail.com>
2022-09-09 22:11:56 +0100

    created basic structure and started work on Feed buttons

---
+++ src/components/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
+
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md:"row"}}}>
+        <Box sx={{height:  { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
+            <Sidebar />
+        </Box>
+    </Stack>
+  )
+}
+
+export default Feed
