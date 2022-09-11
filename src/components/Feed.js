f4dfd31fae0e643f308b7c9d4d0edbc9c4954847
LucasL12345 <lucaslemoine49@gmail.com>
2022-09-11 21:39:55 +0100

    Bug: white page/Nothing displayed, originates from Videos page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
+
 import { Sidebar, Videos } from './'
-import { fetchFromAPI } from '../utils/api'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
-
-  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
+    setVideos(null);
+
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-  }, [])
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md:"row"}}}>
         <Box sx={{height:  { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
-            <Sidebar 
-              selectedCategory = {selectedCategory}
-              setSelectedCategory = {setSelectedCategory}
-            />
+          <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         </Box>
 
         <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
@@ -26,7 +27,7 @@ const Feed = () => {
             {selectedCategory} <span style={{ color: '#F31503'}}>videos</span>
           </Typography>
 
-          <Videos videos={[]} />
+          <Videos videos={videos} />
         </Box>
     </Stack>
   )
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from '@mui/material';
 
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
-}
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos }) => { 
+    return (
+        <Stack flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+          {videos.map((item, idx) => (
+            <Box key={idx}>
+              {item.id.videoId && <VideoCard video={item} /> }
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+          ))}
+        </Stack>
+      );
+    }
 
 export default Videos
+
