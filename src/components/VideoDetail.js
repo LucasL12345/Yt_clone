1813319ea38e1d78c415e3460b88d44805cd0f30
LucasL12345 <lucaslemoine49@gmail.com>
2022-09-17 19:07:14 +0100

    Added bare bones video playing functionality

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]))
+  }, [id]);
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack>
+        <Box>
+          <Box>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
